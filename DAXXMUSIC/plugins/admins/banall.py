d6f2e0d26f805e3f2d8886f361f2236db4809051
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-20 22:42:38 +0530

    Update banall.py

--- DAXXMUSIC/plugins/admins/banall.py
+++ DAXXMUSIC/plugins/admins/banall.py
@@ -1,9 +1,9 @@
-from VIPMUSIC import app
+from DAXXMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from VIPMUSIC.utils.vip_ban import admin_filter
-from VIPMUSIC.misc import SUDOERS
+from DAXXMUSIC.utils.baxx_ban import admin_filter
+from DAXXMUSIC.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
 
